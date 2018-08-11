f9054c05bfe9f9b162eaaa57ffea5cfc38505623
David Eng <twodoorbells@gmail.com>
2018-08-11 19:05:43 -0400

    functional test to check basic input

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -13,16 +15,29 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		# she notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('finish the test!') 
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# she is invited to add a new item right away
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'enter a to-do item'
+		)
 		# she types "buy stuff" into a text box
-
+		inputbox.send_keys('buy stuff')
 		# when she hits enter, the page and lists the item with a number
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1 buy stuff' for row in rows)
+		)
 
 		# there is still a textbox to add a item. she adds "do stuff" and hits enter
-
+		self.fail('finish the test!') 
 		# the page updates again and now shows both items in the list
 
 		# She wants to remember the page and sees that there is a unique URL provided for her
